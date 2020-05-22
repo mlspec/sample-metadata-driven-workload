0f707fc3830b26268c719be71d9cc50cb335afd0
David Aronchick <aronchick@gmail.com>
2020-05-22 21:36:13 +0000

    Initial setup complete

---
+++ tests/test_e2e.py
@@ -0,0 +1,24 @@
+import os
+import logging
+from pathlib import Path
+import yaml as YAML
+import uuid
+import sys
+from mlspeclib import MLObject, MLSchema
+import datetime
+import unittest
+
+sys.path.append(str(Path.cwd()))
+from workflow import main
+
+class E2ETester(unittest.TestCase):
+
+    def test_e2e(self):
+        MLSchema.populate_registry()
+        MLSchema.append_schema_to_registry(Path.cwd() / ".parameters" / "schemas")
+
+        main()
+
+
+if __name__ == "__main__":
+    unittest.main()
