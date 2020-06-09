e40e2cefd027df56751153a8621919dfcfb53870
David Aronchick <aronchick@gmail.com>
2020-06-09 20:24:40 +0000

    adding new workflows

--- create_workflow.py
+++ create_workflow.py
@@ -4,19 +4,20 @@ import yaml as YAML
 import uuid
 import datetime
 from mlspeclib import MLObject, MLSchema, Metastore
-from metastore_credentials import Credentials
+from tests.metastore_credentials import Credentials
 
 sys.path.append(str(Path.cwd()))
 sys.path.append(str(Path.cwd() / "bin"))
 
-from utils import setupLogger  # noqa
+# from utils import setupLogger  # noqa
 
 
 def main():
     MLSchema.append_schema_to_registry(Path(".parameters") / "schemas")
 
-    workflow_version = "500.0.1"
-    workflow_input_string = YAML.safe_dump((Path(".parameters") / "workflow.yaml").read_text())
+    workflow_version = "2.0.0"
+    workflow_path = (Path(".parameters") / "workflow_2_3_0.yaml")
+    workflow_input_string = YAML.safe_dump(workflow_path.read_text())
     workflow_dict = YAML.safe_load(YAML.safe_load(workflow_input_string))
     workflow_dict["workflow_version"] = workflow_version
     workflow_dict["run_id"] = str(uuid.uuid4())
@@ -31,7 +32,7 @@ def main():
 
     workflow_node_id = ms.create_workflow_node(workflow_object, workflow_dict["run_id"])
     ms.create_workflow_steps(workflow_node_id, workflow_object)
-    print(f"Success - workflow_node_id: {workflow_node_id}")
+    print(f"Success {str(workflow_path)} - workflow_node_id: {workflow_node_id}")
 
 
 if __name__ == "__main__":
